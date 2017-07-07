342f9192d3a0555c7c407fed848e2efd6c39fda1
Tomasz Jakut <vepomoc@gmail.com>
2017-07-07 20:46:26 +0200

    refactor($): rewrite code to be compliant with lint rules

--- chai-adapter.js
+++ chai-adapter.js
@@ -1,19 +1,22 @@
-(function(window, karma) {
-  window.should = null;
-  window.should = window.chai.should();
-  window.expect = window.chai.expect;
-  window.assert = window.chai.assert;
+/* eslint no-var: 0 */
 
-  var chaiConfig = karma.config.chai;
-  if (chaiConfig) {
-    for (var key in chaiConfig) {
-      if (chaiConfig.hasOwnProperty(key)) {
-        window.chai.config[key] = chaiConfig[key];
-      }
-    }
-  }
+( function( window, karma ) {
+	window.should = null;
+	window.should = window.chai.should();
+	window.expect = window.chai.expect;
+	window.assert = window.chai.assert;
 
-  karma.loaded = function() {
-    karma.start();
-  };
-})(window, window.__karma__);
+	var chaiConfig = karma.config.chai;
+
+	if ( chaiConfig ) {
+		for ( var key in chaiConfig ) {
+			if ( chaiConfig.hasOwnProperty( key ) ) {
+				window.chai.config[ key ] = chaiConfig[ key ];
+			}
+		}
+	}
+
+	karma.loaded = function() {
+		karma.start();
+	};
+}( window, window.__karma__ ) );
--- index.js
+++ index.js
@@ -1,52 +1,69 @@
-var path = require('path');
+const path = require( 'path' );
 
-var pattern = function(file) {
-  return {pattern: file, included: true, served: true, watched: false};
+function pattern( file ) {
+	return {
+		pattern: file,
+		included: true,
+		served: true,
+		watched: false
+	};
+}
+
+function endsWith( substr ) {
+	return function( str ) {
+		return str.indexOf( substr ) >= 0 && str.indexOf( substr ) === ( str.length - substr.length );
+	};
+}
+
+function _isDuplicate( files, file ) {
+	let result = false;
+
+	for ( let i = 0; i < files.length; i++ ) {
+		if ( files[ i ] ) {
+			const pattern = files[ i ].pattern;
+
+			result = result || endsWith( path.relative( __dirname, file ) )( pattern );
+		}
+	}
+
+	return result;
+}
+
+function framework( files ) {
+	const isDuplicate = _isDuplicate.bind( this, files );
+
+	/* Lolex */
+	const lolexPath = `${ path.dirname( require.resolve( 'lolex/package.json' ) ) }/lolex.js`;
+
+	/* Sinon */
+	const sinonPath = `${ path.dirname( require.resolve( 'sinon/package.json' ) ) }/pkg/sinon.js`;
+
+	if ( !isDuplicate( sinonPath ) ) {
+		files.unshift( pattern( lolexPath ) );
+		files.unshift( pattern( sinonPath ) );
+	}
+
+	/* Chai */
+	const chaiPath = `${ path.dirname( require.resolve( 'chai/package.json' ) ) }/chai.js`;
+
+	if ( !isDuplicate( chaiPath ) ) {
+		files.unshift( pattern( chaiPath ) );
+		files.push( pattern( path.join( __dirname, 'chai-adapter.js' ) ) );
+	}
+
+	/* Sinon-Chai */
+	const sinonChaiPath = path.resolve( require.resolve( 'sinon-chai' ) );
+
+	if ( !isDuplicate( sinonChaiPath ) ) {
+		files.push( pattern( sinonChaiPath ) );
+	}
+}
+
+framework.$inject = [ 'config.files' ];
+
+module.exports = {
+	'framework:sinon-chai': [
+		'factory',
+		framework
+	]
 };
-
-var endsWith = function(substr) {
-  return function(str) {
-    return str.indexOf(substr) >= 0 && str.indexOf(substr) === (str.length - substr.length);
-  };
-};
-
-var _isDuplicate = function(files, file) {
-  var result = false;
-  for (var i = 0; i < files.length; i++) {
-    if (files[i]) {
-      var pattern = files[i].pattern;
-      result = result || endsWith(path.relative(__dirname, file))(pattern);
-    }
-  }
-  return result;
-};
-
-var framework = function(files) {
-  var isDuplicate = _isDuplicate.bind(this, files);
-
-  /* Lolex */
-  var lolexPath = path.dirname(require.resolve('lolex/package.json')) + '/lolex.js';
-
-  /* Sinon */
-  var sinonPath = path.dirname(require.resolve('sinon/package.json')) + '/pkg/sinon.js';
-  if (!isDuplicate(sinonPath)) {
-    files.unshift(pattern(lolexPath));
-    files.unshift(pattern(sinonPath));
-  }
-
-  /* Chai */
-  var chaiPath = path.dirname(require.resolve('chai/package.json')) + '/chai.js';
-  if (!isDuplicate(chaiPath)) {
-    files.unshift(pattern(chaiPath));
-    files.push(pattern(path.join(__dirname, 'chai-adapter.js')));
-  }
-
-  /* Sinon-Chai */
-  var sinonChaiPath = path.resolve(require.resolve('sinon-chai'));
-  if (!isDuplicate(sinonChaiPath)) {
-    files.push(pattern(sinonChaiPath))
-  }
-};
-
-framework.$inject = ['config.files'];
-module.exports = {'framework:sinon-chai': ['factory', framework]};
